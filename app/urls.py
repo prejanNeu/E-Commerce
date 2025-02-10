524c782522bec553ddabbcd29d30e7605a1f3a69
prejan neupane <neupaneprejan2018@gmail.com>
2025-02-10 21:29:24 +0545

    link

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("login/",views.login_page,name="login_page"),
     path("logout/",views.logout_page,name="logout_page"),
     path("send_mail/",views.send_mail,name='send_mail'),
-    path("register/",views.register_page,name="register"),
+    path("register/",views.register_page,name="register_page"),
 ]
