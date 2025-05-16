ece1035fa78c21af6daa2d92ba52da9eee541489
Prejan <neupaneprejan2018@gmail.com>
2025-05-16 17:52:01 +0545

    all

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
-from rest_framework import status 
+from rest_framework import status
 from app.models import CustomUser,Product,Cart
 from .serializers import ProductSerializer,CartSerializer
 from django.contrib.auth.decorators import login_required
@@ -27,7 +27,7 @@ def login_api(request):
 
     if not username or not password :
         return Response({"error": "Username and password required"}, status= status.HTTP_400_BAD_REQUEST)
-    
+
 
     user = authenticate(username=username,password=password)
 
@@ -39,11 +39,10 @@ def login_api(request):
     elif CustomUser.objects.filter(username=username).exists():
 
         return Response({"error":"Password doesnot match"},status=status.HTTP_401_UNAUTHORIZED)
-    
+
 
     else :
         return Response({"error":"user doesnot exist "},status=status.HTTP_404_NOT_FOUND)
-    
 
 
 @api_view(['POST'])
@@ -55,25 +54,26 @@ def register_api(request):
     print(request.data)
     if len(firstName) < 2 or len(lastName) < 2:
         return Response ({"message":"Name must not be empty "},status = status.HTTP_400_BAD_REQUEST)
-    
+
 
     if CustomUser.objects.filter(username=username).exists():
 
         return Response({"message":"username already exists "},status=status.HTTP_409_CONFLICT)
-    
+
     else :
         password = request.data.get("password")
         user = CustomUser.objects.create_user(username=username,password=password)
-        user.is_verified = False 
+        user.is_verified = False
         user.first_name = firstName
         user.last_name = lastName
         user.save()
         return Response({"message":"User create successfully"},status=status.HTTP_201_CREATED)
-    
+
+
 @api_view(['GET'])
 def trending_product(request):
 
-    trending_products = Product.objects.order_by('-sold_count')[:10]  
+    trending_products = Product.objects.order_by('-sold_count')[:10]
     serializer = ProductSerializer(trending_products,many=True)
     return Response(serializer.data)
 
@@ -82,7 +82,8 @@ def trending_product(request):
 def product_view(request):
     ...
 
-@login_required(login_url="/login") 
+
+@login_required(login_url="/login")
 @api_view(['POST'])
 def add_to_cart(request,productId):
     if request.user.is_authenticated:
@@ -99,10 +100,10 @@ def add_to_cart(request,productId):
             return Response({"message": "Cart updated successfully", "quantity": cart_item.quantity}, status=200)
 
         return Response({"message":"Product added to cart "},status=status.HTTP_201_CREATED)
-        
+
     else:
         return Response({"message":"user need to login before cart"},status=status.HTTP_401_UNAUTHORIZED)
-    
+
 
 
 @api_view(['POST'])
@@ -125,29 +126,29 @@ def decrement_cart(request, productId):
             return Response({"message": "Product quantity decremented."}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "Quantity already at zero."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        
+
+
 
 @api_view(['GET'])
 def get_cart(request):
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, status=0).select_related('product')
         serializer = CartSerializer(carts, many=True, context={'request': request})
-        
+
         return Response(serializer.data, status=200)  # Directly returning the list
 
     return Response({'message': 'User not authenticated'}, status=401)
 
 @api_view(['DELETE'])
 def delete_cart(request,cartId):
-    
+
     if Cart.objects.filter(id=cartId).exists():
 
         cart = Cart.objects.get(id=cartId)
         cart.delete()
 
         return Response({'message':"cart delete successfully"},status=status.HTTP_200_OK)
-    
+
     return Response({'error':"cart not found"},status=status.HTTP_404_NOT_FOUND)
 
 
@@ -192,7 +193,7 @@ def esewa_payment_api(request):
     signature = base64.b64encode(digest).decode('utf-8')  # Convert to Base64 string
 
     # Debugging output (optional)
-   
+
     return Response({
         "amount": formatted_total,
         "tax_amount": "0.00",
@@ -206,20 +207,3 @@ def esewa_payment_api(request):
         "signed_field_names": "total_amount,transaction_uuid,product_code",
         "signature": signature,
     },status=status.HTTP_200_OK)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- ePasal/settings.py
+++ ePasal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api',
     'app',
     'rest_framework',
+    'drf_yasg',
 
 ]
 
